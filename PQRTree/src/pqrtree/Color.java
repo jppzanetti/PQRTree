c1309240bcfeda7b26b745fff8b0c27953f84694
João Paulo Pereira Zanetti <jppzanetti@gmail.com>
2017-08-14 10:13:07 -0300

    Javadocs for Color and PQRType.

--- PQRTree/src/pqrtree/Color.java
+++ PQRTree/src/pqrtree/Color.java
@@ -1,8 +1,11 @@
 package pqrtree;
 
 /**
- * Defines the colors a node can have.
- * 
+ * Defines the colors a node can have during the reduction.
+ * A node is colored black when all its descendant leaves are contained in the
+ * added constraint. White is for when the descendant leaves contain none of the
+ * pertinent values or all of them. If neither of those apply, the node is
+ * colored gray.
  * @author Joao
  */
 enum Color {
--- PQRTree/src/pqrtree/PQRType.java
+++ PQRTree/src/pqrtree/PQRType.java
@@ -1,5 +1,10 @@
 package pqrtree;
 
+/**
+ * A simple enum to define the types an internal node can have:
+ * P, Q, or R.
+ * @author Joao
+ */
 enum PQRType {
     P, Q, R;
 }
